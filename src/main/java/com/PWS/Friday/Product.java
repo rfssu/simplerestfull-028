8f68dd14a454b487a8e827b5c83e99d8f8d266a6
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-12-05 18:29:28 +0700

    Initiate Commit

---
+++ src/main/java/com/PWS/Friday/Product.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.PWS.Friday;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+public class Product {
+    private String Id;
+    private String Name;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+    
+    
+}
---
+++ src/test/java/com/PWS/Friday/FridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PWS.Friday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
